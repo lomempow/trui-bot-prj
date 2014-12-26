ff735c298acbf8f7a41e2a45e2d5fba9b645d3e1
lomempow <emily.lomempow@gmail.com>
2014-12-26 21:43:20 +0700

    Add HCTL lib ver1"

---
+++ robot/src/sensor/encoder_hctl.cpp
@@ -0,0 +1,101 @@
+#include "encoder_hctl.h"
+
+using namespace crim;
+
+int64_t result_new_ = 0;
+int64_t result_old_x_ = 0;
+int64_t result_old_y_ = 0;
+int32_t result_ = 0;
+
+EncoderHCTL::EncoderHCTL(size_t en_1_pin, size_t en_2_pin, size_t xy_pin, size_t oe_pin, size_t sel_1_pin, size_t sel_2_pin, size_t reset_x_pin, size_t reset_y_pin)
+    : en_1_pin_(en_1_pin), en_2_pin_(en_2_pin), xy_pin_(xy_pin), oe_pin_(oe_pin), sel_1_pin_(sel_1_pin), sel_2_pin_(sel_2_pin), reset_x_pin_(reset_x_pin), reset_y_pin_(reset_y_pin) {
+
+  pinMode(en_1_pin_, OUTPUT);
+  pinMode(en_2_pin_, OUTPUT);
+  pinMode(xy_pin_, OUTPUT);
+  pinMode(oe_pin_, OUTPUT);
+  pinMode(sel_1_pin_, OUTPUT);
+  pinMode(sel_2_pin_, OUTPUT);
+  DDRB = B00000000;
+
+  digitalWrite(en_1_pin_, HIGH); //EN1 = 1
+  digitalWrite(en_2_pin_, LOW); //EN2 = 0
+  digitalWrite(xy_pin_, LOW); //Select X-axis
+
+}
+
+EncoderHCTL::~EncoderHCTL() {
+
+}
+
+int32_t EncoderHCTL::pos() {
+  digitalWrite(oe_pin_, HIGH); //Disable OE
+  delay(25);
+
+  digitalWrite(sel_1_pin_, LOW); //SEL1 = 0 (MSB)
+  digitalWrite(sel_2_pin_, HIGH); //SEL2 = 1 (MSB)
+  digitalWrite(oe_pin_, LOW); //Enable OE
+  EncoderHCTL::get_hi(); //Get MSB
+
+  digitalWrite(sel_1_pin_, HIGH); //SEL1 = 1 (2nd Byte) 
+  digitalWrite(sel_2_pin_, HIGH); //SEL2 = 1 (2nd Byte) 
+  EncoderHCTL::get_2nd(); //Get 2nd Byte
+
+  digitalWrite(sel_1_pin_, LOW); //SEL1 = 0 (3rd Byte) 
+  digitalWrite(sel_2_pin_, LOW); //SEL2 = 0 (3rd Byte) 
+  EncoderHCTL::get_3rd(); //Get 3rd Byte
+
+  digitalWrite(sel_1_pin_, HIGH); //SEL1 = 1 (LSB) 
+  digitalWrite(sel_2_pin_, LOW); //SEL2 = 0 (LSB) 
+  EncoderHCTL::get_lo(); //Get LSB
+
+  digitalWrite(oe_pin_, HIGH); //Disable OE
+  delay(25);
+
+  result_ = (result_hi_ << 24); //Assign MSB
+  result_ |= (result_2nd_ << 16); //Assign 2nd Byte
+  result_ |= (result_3rd_ << 8); //Assign 3rd Byte
+  result_ |= result_lo_; //Assign LSB
+
+  return result_;
+}
+
+void EncoderHCTL::get_hi() {
+  hi_old_ = PINB;
+  hi_new_ = PINB;
+  if (hi_new_ == hi_old_){
+    result_hi_ = hi_new_;
+  } else {
+    EncoderHCTL::get_hi();
+  }
+}
+
+void EncoderHCTL::get_2nd() {
+  second_old_ = PINB;
+  second_new_ = PINB;
+  if (second_new_ == second_old_){
+    result_2nd_ = second_new_;
+  } else {
+    EncoderHCTL::get_2nd();
+  }
+}
+
+void EncoderHCTL::get_3rd() {
+  third_old_ = PINB;
+  third_new_ = PINB;
+  if (third_new_ == third_old_){
+    result_3rd_ = third_new_;
+  } else {
+    EncoderHCTL::get_3rd();
+  }
+}
+
+void EncoderHCTL::get_lo() {
+  lo_old_ = PINB;
+  lo_new_ = PINB;
+  if (lo_new_ == lo_old_){
+    result_lo_ = lo_new_;
+  } else {
+    EncoderHCTL::get_lo();
+  }
+}
---
+++ robot/src/sensor/encoder_hctl.h
@@ -0,0 +1,63 @@
+#ifndef ENCODER_HCTL_H_
+#define ENCODER_HCTL_H_
+
+#include <arduino/Arduino.h>
+
+namespace crim {
+
+class EncoderHCTL {
+ public:
+  EncoderHCTL(size_t en_1_pin, size_t en_2_pin, size_t xy_pin, size_t oe_pin, size_t sel_1_pin, size_t sel_2_pin, size_t reset_x_pin, size_t reset_y_pin);
+  ~EncoderHCTL();
+
+  /**
+   * @brief return the number of rising-edge of the a phase pulse
+   * 
+   */
+  int32_t pos();
+  
+  /**
+   * @brief return how much rotarion in radians, can be negative
+   * 
+   */
+  double rot();
+  
+ private:
+  size_t en_1_pin_;
+  size_t en_2_pin_;
+  size_t xy_pin_;
+  size_t oe_pin_;
+  size_t sel_1_pin_;
+  size_t sel_2_pin_;
+  size_t reset_x_pin_;
+  size_t reset_y_pin_;
+
+  int8_t hi_old_;
+  int8_t hi_new_;
+  int8_t second_old_;
+  int8_t second_new_;
+  int8_t third_old_;
+  int8_t third_new_;
+  int8_t lo_old_;
+  int8_t lo_new_;
+
+  static int8_t result_hi_;
+  static int8_t result_2nd_;
+  static int8_t result_3rd_;
+  static int8_t result_lo_;
+
+  static int64_t result_new_;
+  static int64_t result_old_x_;
+  static int64_t result_old_y_;
+  static int32_t result_;
+
+  void get_hi();
+  void get_2nd();
+  void get_3rd();
+  void get_lo();
+
+};
+
+}// namespace crim
+
+#endif
